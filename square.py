fc9988cd5766a3957ab950d4c7546819e3950a30
Risto Mononen <leuka.mononen@gmail.com>
2014-11-26 23:33:34 +0200

    Combine assertion with invariant checking. Continuing after invalid invariant does not make sense.

--- square.py
+++ square.py
@@ -1,32 +1,23 @@
 class Square(set):
     min, max = 1, 9
-    
+
     def __init__(self, values=range(min, max+1)):
         super(Square, self).update(values)
-        status, msg = self.invariant()
-        assert status, msg
+        self.assert_invariant()
 
-    def invariant(self):
-        s = self
-        a, b = s.min, s.max
-        n = len(s)
-        if n < a or n > b:
-            return False, 'invalid candidate set size: %d, %s' % (n, s)
-        for x in s:
-            if a <= x <= b:
-                continue
-            return False, '%d out of range [%d, %d]' % (x, a, b)
-        return True, None
+    def assert_invariant(self):
+        n = len(self)
+        assert self.min <= n <= self.max, 'invalid candidate set size: %d, %s' % (n, self)
+        for x in self:
+            assert self.min <= x <= self.max, '%d out of range [%d, %d]' % (x, a, b)
 
     def isdone(self):
         return self.value() != None
 
     def value(self):
-        s = self
-        status, msg = s.invariant()
-        assert status, msg
-        if len(s) == 1:
-            return list(s)[0]
+        self.assert_invariant()
+        if len(self) == 1:
+            return list(self)[0]
         return None
 
 
