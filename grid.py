fd8305712278e053f4839fdca0d46394532c32da
Risto Mononen <leuka.mononen@gmail.com>
2014-12-07 15:50:10 +0200

    Create actual Squares, not just ints

--- grid.py
+++ grid.py
@@ -14,7 +14,7 @@ class Grid(list):
         n = self.box_height
         assert self.box_height == self.box_width
         for i in range(m * m):
-            self.append(i)      # Square()
+            self.append(Square())
         self.row = [self[i*m:(i+1)*m] for i in range(m)]
         self.col = [self[i:m*m:m] for i in range(m)]
         self.box = [[] for i in range(m)]
